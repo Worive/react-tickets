969fcf04f50e8e876cb2f4ad0f4f19343f11919e
Worive <mine.pralito@gmail.com>
2024-07-28 17:08:39 +0200

    run lint:fix

--- resources/js/Pages/Tickets/Dashboard.jsx
+++ resources/js/Pages/Tickets/Dashboard.jsx
@@ -1,14 +1,13 @@
-import {Head} from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.jsx';
 
 export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Tickets - Dashboard</h2>}
+            header={<h2 className="text-xl font-semibold leading-tight text-gray-800">Tickets - Dashboard</h2>}
         >
             <Head title="Tickets Dashboard" />
-
         </AuthenticatedLayout>
     );
-};
+}
