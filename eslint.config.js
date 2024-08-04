1a539d51a1123fd6eeb78251b3d0427107176734
Worive <mine.pralito@gmail.com>
2024-08-04 08:16:45 +0200

    indent

--- eslint.config.js
+++ eslint.config.js
@@ -5,35 +5,38 @@ import pluginPrettier from 'eslint-plugin-prettier';
 import configPrettier from 'eslint-config-prettier';
 
 export default [
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'], // Glob pattern for files to be linted
-    ignores: ['vendor/**/*'], // Exclude vendor directory
-    languageOptions: {
-      globals: globals.browser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+    {
+        files: ['**/*.{js,mjs,cjs,jsx}'], // Glob pattern for files to be linted
+        ignores: ['vendor/**/*'], // Exclude vendor directory
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                route: 'readonly',
+            },
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        plugins: {
+            react: pluginReact,
+            prettier: pluginPrettier,
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'prettier/prettier': [
+                'error',
+                {
+                    endOfLine: 'auto',
+                },
+            ],
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
     },
-    plugins: {
-      react: pluginReact,
-      prettier: pluginPrettier,
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'prettier/prettier': [
-        'error',
-        {
-          endOfLine: 'auto'
-        }
-      ],
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      }
-    }
-  },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat['jsx-runtime'],
-  configPrettier,
+    pluginJs.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    pluginReact.configs.flat['jsx-runtime'],
+    configPrettier,
 ];
